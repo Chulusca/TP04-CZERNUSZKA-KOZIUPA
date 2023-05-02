d7250503246d546520a4cb917e0b0aa505e9463d
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2023-05-02 12:08:44 -0300

    equipos

---
+++ Models/Equipos.cs
@@ -0,0 +1,7 @@
+namespace TP04_CZERNUSZKA_KOZIUPA.Models;
+
+public static class Equipos{
+    public static List<string>ListaEquipos {get;private set;} = new List<string>() {
+        "River", "Boca", "San Lorenzo", "Riestra", "Barracas Central", "Aldosivi", "Estudiantes de la plata", "Instituto"
+    };
+}
---
+++ Models/Indumentaria.cs
@@ -0,0 +1,14 @@
+namespace TP04_CZERNUSZKA_KOZIUPA.Models;
+
+class Indumentaria{
+    public string medias{get;private set;}
+    public string pantalon{get;private set;}
+    public string camiseta{get;private set;}
+
+    public Indumentaria(string m, string p, string c){
+        medias = m; pantalon = p; camiseta = c;
+    }
+    public Indumentaria(){
+        
+    }
+}
