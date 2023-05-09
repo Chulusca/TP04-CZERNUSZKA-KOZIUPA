1e2af0b8093b4713b90453027efacaa8d1bc97ab
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2023-05-09 11:22:04 -0300

    listas

--- Models/Equipos.cs
+++ Models/Equipos.cs
@@ -8,10 +8,10 @@ public static class Equipos{
         "medias_amarillas.png", "medias_azules.png", "medias_blancas.png", "medias_bordo.png", "medias_grises.png", "medias_negras.png", "medias_negras2.png", "medias_rojas", "medias_verdeneon.png", "medias_verde.png"
     };
     public static List<string>ListaPantalones {get;private set;} = new List<string>() {
-        "", "", "", "", "", "", "", "", "", ""
+        "pantalon_azul.png", "pantalon_azuloscuro.png", "pantalon_blanco.png", "pantalon_bordo.png", "pantalon_gris.png", "pantalon_naranja.png", "pantalon_negro.png", "pantalon_rojo.png", "pantalon_rosa.png", "pantalon_violeta.png"
     };
     public static List<string>ListaRemeras {get;private set;} = new List<string>() {
-        "", "", "", "", "", "", "", "", "", ""
+        "remera_amarilla.png", "remera_azul.png", "remera_blanca.png", "remera_celeste.png", "remera_gris.png", "remera_naranja.png", "remera_negra.png", "remera_roja.png", "remera_verde.png", "remera_violeta.png"
     };
     
     public static Dictionary<int, Indumentaria>dicIndumentaria {get; private set;} = new Dictionary<int, Indumentaria>();
